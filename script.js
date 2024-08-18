3d1e1d2f9fbbb5f176a669fb6786531716768d4f
Rishi-Bhati <bhatirishi5@gmail.com>
2024-08-18 11:35:57 +0530

    "Commented out meta viewport tag in index.html and added mobile device warning alert in script.js"

---
+++ script.js
@@ -0,0 +1,3 @@
+if (window.innerWidth < 768) { // adjust this value to your desired threshold
+    window.alert("Important: This website is optimized for desktop devices. Using a mobile device may affect your experience. For the best results, we recommend switching to a desktop or laptop.")  
+}
